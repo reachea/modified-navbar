9faf2763f95d7fd2f10b906abebbcbde76322d1e
Sambath Udomreachea <udomreacheasambath@gmail.com>
2020-12-23 19:44:41 +0700

    hot_fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 
+import styled from "styled-components";
+
+import { Container, Card, Button } from "react-bootstrap";
+
 const App: React.FC<any> = (): any => {
   return (
     <>
-      <div style={{ width: "100%", height: "1500vh" }}>
-        <p>This is me</p>
-      </div>
+      <BodyContainer>
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </BodyContainer>
     </>
   );
 };
 
 export default App;
+
+const BodyContainer = styled(Container)`
+  margin-top: 120px;
+  height: 1500vh;
+`;
