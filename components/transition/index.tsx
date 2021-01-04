9edd94759576bd48164850eec92cfc440a338845
Panhara28 <titpanhara@gmail.com>
2021-01-04 19:48:39 +0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch main  Your branch is up-to-date with 'origin/main'.
     Changes to be committed:
    	modified:   components/transition/index.tsx
    	modified:   pages/index.tsx
    	new file:   yarn.lock

--- components/transition/index.tsx
+++ components/transition/index.tsx
@@ -46,6 +46,8 @@ export class NavContainer extends React.Component<NavContainerProps> {
   }
 
   render() {
+    console.log(this.state.scroll);
+    
     return (
       <>
         <Transition>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,53 @@
-import React from "react";
-
-import { ContentOne } from "../components/content/contentOne";
-import { ContentTwo } from "../components/content/contentTwo";
+import React, { useContext, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { ScrollContext } from "../contexts/scroll";
+import styled from "styled-components";
+import { Button, Card, Container } from "react-bootstrap";
 
 const App: React.FC<any> = (): any => {
+  const arr = [
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+  ]
+  const [element, view] = useInView();
+
+  const scrollOneContext = useContext(ScrollContext);
+
+  useEffect(() => {
+    scrollOneContext.setViewOne(view);
+  }, [view]);
+
   return (
     <>
-      <ContentOne />
-      <ContentTwo />
+      <BodyContainer ref={element}>
+        {
+          arr.map(x => {
+            return(
+              <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            )
+          })
+        }
+      </BodyContainer>
     </>
   );
 };
+
+const BodyContainer = styled(Container)`
+  margin-top: 120px;
+  height: 100vh;
+`;
 
 export default App;
